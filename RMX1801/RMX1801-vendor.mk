b8085c92e6bfa1b1f71665f9cce8bf134f723980
Marco Zanin <mrczn.bb@gmail.com>
2022-03-20 01:20:54 +0100

    RMX1801: Add liblocdiagiface.so [2/2]
    It's needed by GPS logging
    
    Fix: E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library "liblocdiagiface.so" not found
    Change-Id: I36e42ccf67778eef0bde829a11468e5bed7c717a

--- RMX1801/RMX1801-vendor.mk
+++ RMX1801/RMX1801-vendor.mk
@@ -2878,6 +2878,7 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX1801/proprietary/vendor/lib64/libloc_api_v02.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libloc_api_v02.so \
     vendor/realme/RMX1801/proprietary/vendor/lib64/liblocationservice.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblocationservice.so \
     vendor/realme/RMX1801/proprietary/vendor/lib64/liblocationservice_glue.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblocationservice_glue.so \
+    vendor/realme/RMX1801/proprietary/vendor/lib64/liblocdiagiface.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblocdiagiface.so \
     vendor/realme/RMX1801/proprietary/vendor/lib64/liblowi_client.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblowi_client.so \
     vendor/realme/RMX1801/proprietary/vendor/lib64/liblqe.so:$(TARGET_COPY_OUT_VENDOR)/lib64/liblqe.so \
     vendor/realme/RMX1801/proprietary/vendor/lib64/libmdmdetect.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmdmdetect.so \
